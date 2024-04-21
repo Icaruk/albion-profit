c4f9fd0b0f881062e78ae28a655b3cd9f2f85ab8
Icaruk <adri-rk@hotmail.com>
2024-04-21 23:47:39 +0200

    fix: add basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Home from "./pages/home/Home";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
+import { BASENAME } from "../BASENAME";
 
 export default function App() {
 	return (
 		<MantineProvider theme={theme} defaultColorScheme="dark">
-			<BrowserRouter>
+			<BrowserRouter basename={BASENAME}>
 				<Routes>
 					<Route path="/" element={<Home />} />
 				</Routes>
