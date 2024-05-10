87b77f695ff2aed923a448581ded06d200a57c8d
Icaruk <adri-rk@hotmail.com>
2024-05-10 19:26:12 +0200

    loc: missing translations TaxSelector

--- src/pages/home/partials/TaxSelector.jsx
+++ src/pages/home/partials/TaxSelector.jsx
@@ -1,5 +1,6 @@
 import { Select } from "@mantine/core";
 import { IconReceiptTax } from "@tabler/icons-react";
+import * as m from "@/paraglide/messages.js";
 
 export function TaxSelector({ tax, onChange = () => {} }) {
 	return (
@@ -8,11 +9,11 @@ export function TaxSelector({ tax, onChange = () => {} }) {
 			value={String(tax)}
 			onChange={(val) => onChange(+val)}
 			data={[
-				{ value: "0", label: "No tax" },
-				{ value: "4", label: "4% (sell with premium)" },
-				{ value: "8", label: "8% (sell without premium)" },
-				{ value: "6.5", label: "6.5% (sell order with premium)" },
-				{ value: "10.5", label: "10.5% (sell order without premium)" },
+				{ value: "0", label: m.noTax() },
+				{ value: "4", label: m.sellWithPremium({ num: 4 }) },
+				{ value: "8", label: m.sellWIthoutPremium({ num: 8 }) },
+				{ value: "6.5", label: m.sellOrderWithPremium({ num: 6.5 }) },
+				{ value: "10.5", label: m.sellOrderWithoutPremium({ num: 10.5 }) },
 			]}
 			leftSection={<IconReceiptTax />}
 		/>
